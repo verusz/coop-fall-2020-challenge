adc0bcc2dccf8b5f9419496e45f4a2c5f794813b
verusz <523726352@qq.com>
2020-07-10 14:12:42 -0400

    passed all cases

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,68 @@
+
+class Event():
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
 class EventSourcer():
     # Do not change the signature of any functions
 
     def __init__(self):
         self.value = 0
+        self.redoEvents = []
+        self.events = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.events.append(Event("add", num))
+        
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.events.append(Event("subtract", num))
+        
 
     def undo(self):
-        pass
+        if (self.events):
+            last = self.events.pop()
+            self.undoAction(last)
+            self.redoEvents.append(last)
+        
 
     def redo(self):
-        pass
+        if (self.redoEvents):
+            first = self.redoEvents.pop(0)
+            self.redoAction(first)
+            self.events.append(first)
+        
 
     def bulk_undo(self, steps: int):
-        pass
+        i = 0
+        while (self.events and i < steps):
+            event = self.events.pop()
+            self.undoAction(event)
+            self.redoEvents.append(event)
+            i += 1
 
     def bulk_redo(self, steps: int):
-        pass
+        i = 0
+        while (self.redoEvents and i < steps): 
+            event = self.redoEvents.pop(0)
+            self.redoAction(event)
+            self.events.append(event)
+            i += 1
+
+
+    def undoAction(self, event: Event):# reverse action if undo
+        if event.name == "subtract":
+            self.value += event.value
+        if event.name == "add":
+            self.value -= event.value
+
+    def redoAction(self, event: Event):
+        if event.name == "add":
+            self.value += event.value
+        if event.name == "subtract":
+            self.value -= event.value
+
+
